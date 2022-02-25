bc3894842845eaa66923d181f9497c499fdcf84d
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-02-25 18:19:39 -0500

    started sc work

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,9 @@
 module Sable
 
 go 1.16
+
+require (
+	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // direct
+	golang.org/x/mobile v0.0.0-20220224134551-8a0a1e50732f // direct
+	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,7 +1,57 @@
 package main
 
-import()
+import(
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/event/mouse"
+	"golang.org/x/mobile/event/key"
+
+	"github.com/kbinani/screenshot"
+)
 
 func main(){
-	
+	keyAltPressed, keyHPressed := false, false
+	var e1, e2 mouse.Event = mouse.Event{}, mouse.Event{}
+	app.Main(func(a app.App){ 
+		for e := range a.Events(){
+			switch e := a.Filter(e).(type) {
+				case key.Code:
+					switch e {
+						case key.CodeLeftAlt:
+							//start the key combo if alt pressed
+							keyAltPressed = true
+						case key.CodeH:
+							//marks h as pressed
+							keyHPressed = true
+						default:
+							//exit the combo if any other key is pressed
+							keyAltPressed = false
+							keyHPressed = false
+					}
+				case mouse.Event:
+					if keyAltPressed && keyHPressed && (e2 == mouse.Event{}) {
+						// if the keys have been pressed, and e2 isnt assigned yet
+						e1 = e
+					} else if keyAltPressed && keyHPressed && (e1 == mouse.Event{}) {
+						// if the keys have been pressed, and e1 is assigned
+						e2 = e
+						screenshotThis(e1, e2)
+					}
+			}
+		}
+	}) 
+}
+
+func getMouseLoc(e mouse.Event) (int, int){
+	return int(e.X), int(e.Y)
+}
+
+func screenshotThis(e1 mouse.Event, e2 mouse.Event) {
+	//get the mouse location
+	x1, y1 := getMouseLoc(e1)
+	x2, y2 := getMouseLoc(e2)
+	//get the screenshot
+	img, err := screenshot.Capture(x1, y1, x2-x1, y2-y1)
+	if err != nil {
+		panic(err)
+	}
 }
