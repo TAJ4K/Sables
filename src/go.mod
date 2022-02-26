1cb2d3c4d1f7d6cd5e921e754a0998376446e16d
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-02-25 19:16:59 -0500

    input detection + screenshotting

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // direct
-	golang.org/x/mobile v0.0.0-20220224134551-8a0a1e50732f // direct
+	github.com/moutend/go-hook v0.1.0
 	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,57 +1,117 @@
 package main
 
-import(
-	"golang.org/x/mobile/app"
-	"golang.org/x/mobile/event/mouse"
-	"golang.org/x/mobile/event/key"
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/moutend/go-hook/pkg/keyboard"
+	"github.com/moutend/go-hook/pkg/mouse"
+	"github.com/moutend/go-hook/pkg/types"
 
 	"github.com/kbinani/screenshot"
 )
 
 func main(){
-	keyAltPressed, keyHPressed := false, false
-	var e1, e2 mouse.Event = mouse.Event{}, mouse.Event{}
-	app.Main(func(a app.App){ 
-		for e := range a.Events(){
-			switch e := a.Filter(e).(type) {
-				case key.Code:
-					switch e {
-						case key.CodeLeftAlt:
-							//start the key combo if alt pressed
-							keyAltPressed = true
-						case key.CodeH:
-							//marks h as pressed
-							keyHPressed = true
-						default:
-							//exit the combo if any other key is pressed
-							keyAltPressed = false
-							keyHPressed = false
-					}
-				case mouse.Event:
-					if keyAltPressed && keyHPressed && (e2 == mouse.Event{}) {
-						// if the keys have been pressed, and e2 isnt assigned yet
-						e1 = e
-					} else if keyAltPressed && keyHPressed && (e1 == mouse.Event{}) {
-						// if the keys have been pressed, and e1 is assigned
-						e2 = e
-						screenshotThis(e1, e2)
-					}
+	_, err := os.Stat(getAppData()) 
+	if err != nil {
+		fmt.Println("Creating directory")
+		err = os.Mkdir(getAppData(), 0777)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error{
+	keyAltPressed := false
+	keyAlt, keyH := "VK_LMENU", "VK_H"
+
+	keyboardChan := make(chan types.KeyboardEvent, 100)
+	if err := keyboard.Install(nil, keyboardChan); err != nil {
+		return err
+	}
+
+	defer keyboard.Uninstall()
+
+	fmt.Println("start capturing keyboard input")
+
+	for {
+		select {
+		case e := <-keyboardChan:
+			if e.VKCode.String() == keyAlt {
+				fmt.Println("Alt pressed")
+				keyAltPressed = true
+			}
+			if e.VKCode.String() == keyH && keyAltPressed {
+				fmt.Println("H pressed")
+				if err := mouseListener(); err != nil {
+					return err
+				}
+				keyAltPressed = false
 			}
 		}
-	}) 
+	}
 }
 
-func getMouseLoc(e mouse.Event) (int, int){
-	return int(e.X), int(e.Y)
+func mouseListener() error{
+	var mousePos1 types.MouseEvent
+
+	mouseChan := make(chan types.MouseEvent)
+
+	if err := mouse.Install(nil, mouseChan); err != nil {
+		return err
+	}
+
+	defer mouse.Uninstall()
+
+	for {
+		select {
+			case e := <-mouseChan:
+				if e.Message.String() == "Message(513)" {
+					fmt.Println("Left button pressed")
+					mousePos1 = e
+				} else if e.Message.String() == "Message(514)" {
+					fmt.Println("Left button released")
+					screenshotThis(mousePos1, e)
+					return nil
+				}
+		}
+	}
+	
 }
 
-func screenshotThis(e1 mouse.Event, e2 mouse.Event) {
+func getMouseLoc(m types.MouseEvent) (int, int){
+	return int(m.X), int(m.Y)
+}
+
+func getAppData() string {
+	basePath, err := os.UserConfigDir()
+	if err != nil {
+		panic(err)
+	}
+	return basePath + "\\Sable"
+}
+
+func screenshotThis(e1 types.MouseEvent, e2 types.MouseEvent) {
 	//get the mouse location
 	x1, y1 := getMouseLoc(e1)
 	x2, y2 := getMouseLoc(e2)
+
+	fmt.Println(x1, y1, x2, y2)
 	//get the screenshot
 	img, err := screenshot.Capture(x1, y1, x2-x1, y2-y1)
 	if err != nil {
 		panic(err)
 	}
+	file, err := os.Create(getAppData() + "\\screenshot.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	png.Encode(file, img)
 }
